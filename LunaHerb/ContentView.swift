0ee90eb61b355cd12b06559db95637a1dcaf716e
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-02-11 13:47:23 +0100

    setup project

---
+++ LunaHerb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LunaHerb
+//
+//  Created by Rebecca Calabretta on 11.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LunaHerb/LunaHerbApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaHerbApp.swift
+//  LunaHerb
+//
+//  Created by Rebecca Calabretta on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct LunaHerbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
