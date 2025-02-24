790e8cca12e51fcf6ad2470288d2c2eea11d1215
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-02-24 12:40:49 +0100

    HerbData und Json angelegt

---
+++ LunaHerb/Models/HerbData.swift
@@ -0,0 +1,25 @@
+//
+//  HerbData.swift
+//  LunaHerb
+//
+//  Created by Rebecca Calabretta on 23.02.25.
+//
+
+import Foundation
+
+struct HerbData: Identifiable, Codable {
+    var id: UUID
+    var name: String
+    var description: String
+    var asyncImageURL: String
+    var properties: [String]
+    var effect: String
+    var usage: [String]
+    var symptoms: [String]
+    var ingredients: [String]
+    var sideEffects: [String]?
+    var contraindications: [String]?
+    var harvestTime: String
+    var harvest: String
+    var isFavorite: Bool
+}
