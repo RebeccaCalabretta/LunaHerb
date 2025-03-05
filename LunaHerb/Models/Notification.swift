70cf8c5ea1d36649934010129b22e15f46c7edce
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-03-05 14:05:25 +0100

    Notification Model erstellt

---
+++ LunaHerb/Models/Notification.swift
@@ -0,0 +1,15 @@
+//
+//  Notification.swift
+//  LunaHerb
+//
+//  Created by Rebecca Calabretta on 05.03.25.
+//
+
+import Foundation
+
+struct Notification {
+    var id: UUID = UUID()
+    var title: String
+    var message: String
+    var date: Date
+}
