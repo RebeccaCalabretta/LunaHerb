60e28b62653a0500a8b3c2e4c9cd7b7e5663e251
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-03-19 23:57:44 +0100

    Onboarding

---
+++ LunaHerb/Models/NavigationBarTitle.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBarTitle.swift
+//  LunaHerb
+//
+//  Created by Rebecca Calabretta on 19.03.25.
+//
+
+import SwiftUI
+
+struct NavigationBarTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBarTitle()
+}
